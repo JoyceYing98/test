abe138d1858459df2a20b9e3f617930ef39d550c
JoyceYing98 <66371194+JoyceYing98@users.noreply.github.com>
2020-06-10 12:26:52 +0800

    Update insertion.php

--- insertion.php
+++ insertion.php
@@ -1,3 +1,21 @@
 <?
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "joyce", "pwd" => "1998117Ckw", "Database" => "webapplicationtp042403", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:webapplicationtp042403.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 
+if(!$conn)
+  {
+    die("Error connection: ".sqlsrv_errors());
+  }
+
+ $sql = "INSERT INTO restaurant (restaurant_name, restaurant_address, restaurant_phone) VALUES (?, ?, ?)";
+  $params = array("Burger King", "Bukit Jalil", "03-11223444");
+
+ 
+
+  $stmt = sqlsrv_query( $conn, $sql, $params);
+  if( $stmt === false ) {
+        die( print_r( sqlsrv_errors(), true));
+   }
 ?>
