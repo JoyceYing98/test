79f72f44d568f9a2f6f9019fee2b635e85684ab2
JoyceYing98 <66371194+JoyceYing98@users.noreply.github.com>
2020-06-10 12:14:00 +0800

    Update testview.php

--- testview.php
+++ testview.php
@@ -1,6 +1,6 @@
 <?php
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "joyce", "pwd" => "{your_password_here}", "Database" => "webapplicationtp042403", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "joyce", "pwd" => "1998117Ckw", "Database" => "webapplicationtp042403", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:webapplicationtp042403.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
